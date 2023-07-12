d7ba2f44594fbeea81472d0245a3659ab7341b80
Harsha <shreeharsha.a@improve10x.com>
2023-07-12 16:24:55 +0530

    Completed fizzbuzz

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -3,10 +3,14 @@ package fizzbuzz;
 public class FizzBuzz {
 
         public String findFizzBuzz(int n) {
-        String result = "";
-        if (n % 3 == 0) {
+        String result = String.valueOf(n);
+        if (n%3 == 0 && n%5==0) {
+            result = "FIZZBUZZ";
+        }else if(n % 3 == 0) {
             result = "FIZZ";
+        } else if (n %5==0) {
+            result = "BUZZ";
         }
-        return result;
+            return result;
     }
 }
--- src/fizzbuzz/FizzBuzzTest.java
+++ src/fizzbuzz/FizzBuzzTest.java
@@ -10,10 +10,30 @@ public class FizzBuzzTest {
     public void nothing() {
     }
 
+    @Test
+    public void checkThreeAndFiveNot() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.findFizzBuzz(23);
+        assertEquals("23", result);
+    }
    @Test
     public void checkThree() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         String result = fizzBuzz.findFizzBuzz(9);
         assertEquals("FIZZ", result);
+   }
+
+   @Test
+    public void checkFive() {
+       FizzBuzz fizzBuzz = new FizzBuzz();
+       String result = fizzBuzz.findFizzBuzz(10);
+       assertEquals("BUZZ", result);
+   }
+
+   @Test
+    public void checkThreeAndFive() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.findFizzBuzz(15);
+        assertEquals("FIZZBUZZ", result);
    }
 }
