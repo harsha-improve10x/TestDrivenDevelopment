6942d62671a7f5bda256da90a74af2091b791c95
Harsha <shreeharsha.a@improve10x.com>
2023-07-14 11:19:28 +0530

    Checked for an empty list it is returning 0.

---
+++ src/FindDifferenceMaximumAndMinimumArrays/Difference.java
@@ -0,0 +1,7 @@
+package FindDifferenceMaximumAndMinimumArrays;
+
+public class Difference {
+    public int findDiff(int[] arr) {
+        return 0;
+    }
+}
--- src/FindDifferenceMaximumAndMinimumArrays/DifferenceTest.java
+++ src/FindDifferenceMaximumAndMinimumArrays/DifferenceTest.java
@@ -2,9 +2,27 @@ package FindDifferenceMaximumAndMinimumArrays;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DifferenceTest {
 
     @Test
     public void nothing() {
+    }
+    /*
+    Create a function that takes an array and returns the difference between the biggest and smallest numbers.
+    example:-differenceMaxMin([10, 4, 1, 4, -10, -50, 32, 21]) ➞ 82
+    Smallest number is -50, biggest is 32.
+
+    differenceMaxMin([44, 32, 86, 19]) ➞ 67
+    Smallest number is 19, biggest is 86.
+     */
+
+    @Test
+    public void empty() {
+        Difference difference = new Difference();
+        int result = difference.findDiff(new int[]{});
+        assertEquals(0,result);
     }
 }
