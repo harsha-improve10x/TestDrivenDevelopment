94311e370bc15470ffc8e9c9d186696cfa4e9359
Harsha <shreeharsha.a@improve10x.com>
2023-07-16 12:34:38 +0530

    Tested for givenMinusFour_returnMinusOne.

---
+++ src/NthEvenNumber/NthEvenNumber.java
@@ -0,0 +1,11 @@
+package NthEvenNumber;
+
+public class NthEvenNumber {
+    public int even(int n) {
+        int result = 0;
+        if (n != 0) {
+            result = -1;
+        }
+        return result;
+    }
+}
--- src/NthEvenNumber/NthEvenNumberTest.java
+++ src/NthEvenNumber/NthEvenNumberTest.java
@@ -2,8 +2,31 @@ package NthEvenNumber;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class NthEvenNumberTest {
 
+    /*
+    Problem: Create a function that takes a number n
+             and returns the nth even number beginning with 0 as the first.
+    Example: even(1) →0
+    even(2) →2
+    even(100) →198
+     */
     @Test
     public void nothing(){}
+
+    @Test
+    public void givenEmpty_returnsZero() {
+        NthEvenNumber nthEvenNumber = new NthEvenNumber();
+        int result = nthEvenNumber.even(0);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void givenMinusFour_returnMinusOne() {
+        NthEvenNumber nthEvenNumber = new NthEvenNumber();
+        int result = nthEvenNumber.even(-4);
+        assertEquals(-1, result);
+    }
 }
