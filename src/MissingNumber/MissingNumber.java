45d13c653aeef102cf4637e08c00c3ce856c11b5
Harsha <shreeharsha.a@improve10x.com>
2023-07-17 14:59:27 +0530

    Created a package and class for missing Number and tested for testcases.

---
+++ src/MissingNumber/MissingNumber.java
@@ -0,0 +1,20 @@
+package MissingNumber;
+
+public class MissingNumber {
+    public int findMissingNumber(int[] arr) {
+        int missingNumber = 0;
+
+        if (arr.length == 0) {
+            missingNumber = 0;
+        }else if(arr[0] <= 0) {
+            missingNumber = -1;
+        } else if(arr.length > 0 && arr.length < 9){
+            missingNumber = 0;
+        } else if(arr.length == 9) {
+            int sum = 55;
+            int total = arr[0] + arr[1] + arr[2] + arr[3] + arr[4] + arr[5] + arr[6] + arr[7] + arr[8];
+            missingNumber = sum - total;
+        }
+        return missingNumber;
+    }
+}
--- src/MissingNumber/MissingNumberTest.java
+++ src/MissingNumber/MissingNumberTest.java
@@ -1,9 +1,62 @@
 package MissingNumber;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MissingNumberTest {
+    private MissingNumber missingNumber;
+
+    /*
+    Problem:    Create a method that takes an array of integers between 1 and 10
+                (excluding one number) and returns the missing number.
+    Example:    missingNum([1, 2, 3, 4, 6, 7, 8, 9, 10]) ➞ 5
+                missingNum([7, 2, 3, 6, 5, 9, 1, 4, 8]) ➞ 10
+                missingNum([10, 5, 1, 2, 4, 6, 8, 3, 9]) ➞ 7
+     */
 
     @Test
     public void nothing() {}
+
+    @BeforeEach
+    public void setup() {
+        missingNumber = new MissingNumber();
+    }
+
+    @Test
+    public void givenEmpty_returnZero() {
+        int result = missingNumber.findMissingNumber(new int[]{});
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void givenNegativeNumbers_returnMinusOne() {
+        int result = missingNumber.findMissingNumber(new int[]{-2});
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void givenOneNumber_returnZero() {
+        int result = missingNumber.findMissingNumber(new int[]{1});
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void givenNumbers_returnZero() {
+        int result = missingNumber.findMissingNumber(new int[]{1, 3, 4, 2, 7, 6, 9, 8});
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void givenNumbers_returnTen() {
+        int result = missingNumber.findMissingNumber(new int[]{1, 3, 4, 2, 7, 6, 9, 8, 5});
+        assertEquals(10, result);
+    }
+
+    @Test
+    public void givenNumbers_returnSeven() {
+        int result = missingNumber.findMissingNumber(new int[]{10, 5, 1, 2, 4, 6, 8, 3, 9});
+        assertEquals(7, result);
+    }
 }
