468397802bb3b78eab0e8226efe18f7dfdc2e26d
Harsha <shreeharsha.a@improve10x.com>
2023-07-18 15:45:39 +0530

    Tested for givenNull_returnZero

---
+++ src/countvowels/CountVowels.java
@@ -0,0 +1,7 @@
+package countvowels;
+
+public class CountVowels {
+    public int count(String s) {
+        return 0;
+    }
+}
--- src/countvowels/CountVowelsTest.java
+++ src/countvowels/CountVowelsTest.java
@@ -2,8 +2,18 @@ package countvowels;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CountVowelsTest {
 
     @Test
     public void nothing() {}
+
+    @Test
+    public void givenNull_returnZero() {
+        CountVowels countVowels = new CountVowels();
+        int result = countVowels.count("");
+        assertEquals(0, result);
+    }
+
 }
