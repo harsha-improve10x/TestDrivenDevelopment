b47613eb5b412b4d65fc9c05a8181598fc300cd5
Harsha <shreeharsha.a@improve10x.com>
2023-07-18 15:51:29 +0530

    Tested for givenA_returnOne

--- src/countvowels/CountVowels.java
+++ src/countvowels/CountVowels.java
@@ -2,6 +2,9 @@ package countvowels;
 
 public class CountVowels {
     public int count(String s) {
+        if (s.contains("A")) {
+            return 1;
+        }
         return 0;
     }
 }
--- src/countvowels/CountVowelsTest.java
+++ src/countvowels/CountVowelsTest.java
@@ -1,19 +1,29 @@
 package countvowels;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CountVowelsTest {
-
+    private CountVowels countVowels;
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        countVowels = new CountVowels();
+    }
+
     @Test
     public void givenNull_returnZero() {
-        CountVowels countVowels = new CountVowels();
         int result = countVowels.count("");
         assertEquals(0, result);
     }
 
+    @Test
+    public void givenA_returnOne() {
+        int result = countVowels.count("A");
+        assertEquals(1, result);
+    }
 }
