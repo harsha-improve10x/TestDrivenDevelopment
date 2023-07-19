cbaa4ee5edbb5dfa544992dd1fdacf18dae7ba5a
Harsha <shreeharsha.a@improve10x.com>
2023-07-19 11:51:01 +0530

    tested for givenTrue_returnOne, givenTrueFalse_returnOne, givenTrueFalseTruetrue_returnThree

--- src/HowMuchIsTrue/HowMuchIsTrue.java
+++ src/HowMuchIsTrue/HowMuchIsTrue.java
@@ -2,6 +2,14 @@ package HowMuchIsTrue;
 
 public class HowMuchIsTrue {
     public int countTrue(String[] string) {
-        return 0;
+        int count = 0;
+        if (string.length > 0) {
+            for (int i = 0; i < string.length; i++) {
+                if (string[i].equalsIgnoreCase("true")) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
--- src/HowMuchIsTrue/HowMuchIsTrueTest.java
+++ src/HowMuchIsTrue/HowMuchIsTrueTest.java
@@ -1,18 +1,42 @@
 package HowMuchIsTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class HowMuchIsTrueTest {
+    private HowMuchIsTrue howMuchIsTrue;
 
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        howMuchIsTrue = new HowMuchIsTrue();
+    }
+
     @Test
     public void givenNull_returnZero() {
-        HowMuchIsTrue howMuchIsTrue = new HowMuchIsTrue();
         int result = howMuchIsTrue.countTrue(new String[]{""});
         assertEquals(0, result);
+    }
+
+    @Test
+    public void givenTrue_returnOne() {
+        int result = howMuchIsTrue.countTrue(new String[]{"True"});
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void givenTrueFalse_returnOne() {
+        int result = howMuchIsTrue.countTrue(new String[]{"True","False"});
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void givenTrueFalseTruetrue_returnThree() {
+        int result = howMuchIsTrue.countTrue(new String[]{"True","False","True","true"});
+        assertEquals(3, result);
     }
 }
