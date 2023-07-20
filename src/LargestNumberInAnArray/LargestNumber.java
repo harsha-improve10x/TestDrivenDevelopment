b7f503086ad0c2ee972ff32c4f7251b1c97e9c5b
Harsha <shreeharsha.a@improve10x.com>
2023-07-20 12:49:32 +0530

    tested for givenOne_returnOne

--- src/LargestNumberInAnArray/LargestNumber.java
+++ src/LargestNumberInAnArray/LargestNumber.java
@@ -2,6 +2,10 @@ package LargestNumberInAnArray;
 
 public class LargestNumber {
     public int findLargestNumber(int[] arr) {
-        return 0;
+        int largest = 0;
+        if (arr.length > 0) {
+            largest = arr[0];
+        }
+        return largest;
     }
 }
--- src/LargestNumberInAnArray/LargestNumberTest.java
+++ src/LargestNumberInAnArray/LargestNumberTest.java
@@ -1,18 +1,29 @@
 package LargestNumberInAnArray;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LargestNumberTest {
-
+    private LargestNumber largestNumber;
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        largestNumber = new LargestNumber();
+    }
+
     @Test
     public void givenEmpty_returnZero() {
-        LargestNumber largestNumber = new LargestNumber();
         int result = largestNumber.findLargestNumber(new int[]{});
         assertEquals(0, result);
+    }
+
+    @Test
+    public void givenOne_returnOne() {
+        int result = largestNumber.findLargestNumber(new int[]{1});
+        assertEquals(1, result);
     }
 }
