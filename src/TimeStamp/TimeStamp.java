ec8641c8afc67464a1b9598e5dc445edbed150d8
Harsha <shreeharsha.a@improve10x.com>
2023-07-25 17:06:37 +0530

    Implemented spike pit

--- src/TimeStamp/TimeStamp.java
+++ src/TimeStamp/TimeStamp.java
@@ -1,6 +1,15 @@
 package TimeStamp;
 
+import java.time.Instant;
+import java.util.Date;
+
 public class TimeStamp {
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        long l = date.getTime();
+        System.out.println(l);
+    }
     public String getDisplayDate(long timeStampInMillis) {
         return "";
     }
--- src/TimeStamp/TimeStampTest.java
+++ src/TimeStamp/TimeStampTest.java
@@ -1,24 +1,35 @@
 package TimeStamp;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TimeStampTest {
-
+    private TimeStamp timeStamp;
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        timeStamp = new TimeStamp();
+    }
+
+
     @Test
     public void givenZero_whenGetDisplayDateCalled_returnEmptyString() {
-        TimeStamp timeStamp = new TimeStamp();
         String  result = timeStamp.getDisplayDate(0l);
         assertEquals("", result);
     }
 
     @Test
     public void givenMinusTwelve_whenGetDisplayDateCalled_returnEmptyString() {
-        TimeStamp timeStamp = new TimeStamp();
+        String  result = timeStamp.getDisplayDate(-12);
+        assertEquals("", result);
+    }
+
+    @Test
+    public void givenGetCurrentDate_whenGetDisplayDateCalled_returnDate() {
         String  result = timeStamp.getDisplayDate(-12);
         assertEquals("", result);
     }
