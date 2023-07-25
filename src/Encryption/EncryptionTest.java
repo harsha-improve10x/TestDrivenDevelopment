fd6a35d33a4fb09b2f3a6116b3c85089f692ac7a
Harsha <shreeharsha.a@improve10x.com>
2023-07-25 18:40:43 +0530

    Tested for givenEmpty_returnAca

--- src/Encryption/EncryptionTest.java
+++ src/Encryption/EncryptionTest.java
@@ -1,18 +1,30 @@
 package Encryption;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class EncryptionTest {
-
+    private Encryption encryption;
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        encryption = new Encryption();
+    }
+
     @Test
     public void givenNull_returnAca() {
-        Encryption encryption = new Encryption();
         String result = encryption.encrypt(null);
         assertEquals("aca", result);
     }
+
+    @Test
+    public void givenEmpty_returnAca() {
+        String result = encryption.encrypt(null);
+        assertEquals("aca", result);
+    }
+
 }
