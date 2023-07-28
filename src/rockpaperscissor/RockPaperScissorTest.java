40feeada0018f2beee34f211b5735fccdd5a0935
Harsha <shreeharsha.a@improve10x.com>
2023-07-28 12:11:13 +0530

    Tested for givenRockPaper_returnPlayer2Wins

--- src/rockpaperscissor/RockPaperScissorTest.java
+++ src/rockpaperscissor/RockPaperScissorTest.java
@@ -1,32 +1,78 @@
 package rockpaperscissor;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class RockPaperScissorTest {
-
+    private  RpsGame rpsGame;
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        rpsGame = new RpsGame();
+    }
+
     @Test
     public void givenBothNull_returnNull() {
-        RpsGame rpsGame = new RpsGame();
         String result = rpsGame.rps(null, null);
         assertEquals("", result);
     }
 
     @Test
     public void givenOneEmptyOneNull_returnEmpty() {
-        RpsGame rpsGame = new RpsGame();
         String result = rpsGame.rps("", null);
         assertEquals("", result);
     }
 
     @Test
     public void givenTwoEmpty_returnEmpty() {
-        RpsGame rpsGame = new RpsGame();
         String result = rpsGame.rps("", "");
         assertEquals("", result);
     }
+
+    @Test
+    public void givenRockEmpty_returnEmpty() {
+        String result = rpsGame.rps("Rock", "");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void givenEmptyRock_returnEmpty() {
+        String result = rpsGame.rps("", "rock");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void givenPaperEmpty_returnEmpty() {
+        String result = rpsGame.rps("paper", "");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void givenEmptyPaper_returnEmpty() {
+        String result = rpsGame.rps("", "paper");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void givenScissorEmpty_returnEmpty() {
+        String result = rpsGame.rps("Scissor", "");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void givenEmptyScissors_returnEmpty() {
+        String result = rpsGame.rps("", "Scissor");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void givenRockPaper_returnPlayer2Wins() {
+        String result = rpsGame.rps("Rock", "Paper");
+        assertEquals("Player2 Wins", result);
+    }
+
 }
--- src/rockpaperscissor/RpsGame.java
+++ src/rockpaperscissor/RpsGame.java
@@ -2,6 +2,12 @@ package rockpaperscissor;
 
 public class RpsGame {
     public String rps(String player1, String player2) {
-        return "";
+        String result = "";
+        if (player1 == null || player2 == null) {
+            result = "";
+        } else if (player1.equalsIgnoreCase("Rock") && player2.equalsIgnoreCase("paper")) {
+            result = "Player2 Wins";
+        }
+        return result;
     }
 }
