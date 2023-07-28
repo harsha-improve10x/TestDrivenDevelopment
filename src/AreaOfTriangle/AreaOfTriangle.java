65126e5ad35c2c17b78048934a1181dcff361ce6
Harsha <shreeharsha.a@improve10x.com>
2023-07-28 17:30:22 +0530

    Tested for givenSevenAndFour_returnFourteen

--- src/AreaOfTriangle/AreaOfTriangle.java
+++ src/AreaOfTriangle/AreaOfTriangle.java
@@ -2,12 +2,13 @@ package AreaOfTriangle;
 
 public class AreaOfTriangle {
     public int findTriArea(int a, int b) {
-        int result = Integer.MIN_VALUE;
+        int result = 0;
         if (a < 0 || b < 0) {
-            return result;
-        }
-        if (a == 0 || b == 0) {
+            result = -1;
+        }else if (a == 0 || b == 0) {
             result = 0;
+        } else if (a > 0 || b > 0) {
+            result = ((a * b)/2);
         }
         return result;
     }
--- src/AreaOfTriangle/AreaOfTriangleTest.java
+++ src/AreaOfTriangle/AreaOfTriangleTest.java
@@ -1,32 +1,48 @@
 package AreaOfTriangle;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AreaOfTriangleTest {
+    private AreaOfTriangle areaOfTriangle;
 
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        areaOfTriangle = new AreaOfTriangle();
+    }
+
     @Test
     public void givenZero_returnZero() {
-        AreaOfTriangle areaOfTriangle = new AreaOfTriangle();
         int result = areaOfTriangle.findTriArea(0, 0);
         assertEquals(0, result);
     }
 
     @Test
     public void givenMinusOneZero_returnMinValue() {
-        AreaOfTriangle areaOfTriangle = new AreaOfTriangle();
         int result = areaOfTriangle.findTriArea(-1, 0);
-        assertEquals(Integer.MIN_VALUE, result);
+        assertEquals(-1, result);
     }
 
     @Test
     public void givenMinusOneMinusTwo_returnMinValue() {
-        AreaOfTriangle areaOfTriangle = new AreaOfTriangle();
         int result = areaOfTriangle.findTriArea(-1, -2);
-        assertEquals(Integer.MIN_VALUE, result);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void givenThreeAndTwo_returnThree() {
+        int result = areaOfTriangle.findTriArea(3, 2);
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void givenSevenAndFour_returnFourteen() {
+        int result = areaOfTriangle.findTriArea(7, 4);
+        assertEquals(14, result);
     }
 }
