037b6a542ed9ca169173257f3e1c42f37e048fc1
Harsha <shreeharsha.a@improve10x.com>
2023-07-28 17:19:32 +0530

    Tested for givenZero_returnZero

---
+++ src/AreaOfTriangle/AreaOfTriangle.java
@@ -0,0 +1,7 @@
+package AreaOfTriangle;
+
+public class AreaOfTriangle {
+    public int findTriArea(int a, int b) {
+        return 0;
+    }
+}
--- src/AreaOfTriangle/AreaOfTriangleTest.java
+++ src/AreaOfTriangle/AreaOfTriangleTest.java
@@ -2,8 +2,17 @@ package AreaOfTriangle;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AreaOfTriangleTest {
 
     @Test
     public void nothing() {}
+
+    @Test
+    public void givenZero_returnZero() {
+        AreaOfTriangle areaOfTriangle = new AreaOfTriangle();
+        int result = areaOfTriangle.findTriArea(0, 0);
+        assertEquals(0, result);
+    }
 }
