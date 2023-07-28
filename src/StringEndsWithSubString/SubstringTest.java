9324e2737d3d9784d8e221c15285183701dcb109
Harsha <shreeharsha.a@improve10x.com>
2023-07-28 15:09:53 +0530

    Tested for givenEmpty_returnEmpty

--- src/StringEndsWithSubString/SubstringTest.java
+++ src/StringEndsWithSubString/SubstringTest.java
@@ -1,18 +1,29 @@
 package StringEndsWithSubString;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SubstringTest {
+    private Substring substring;
 
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        substring = new Substring();
+    }
     @Test
     public void givenNull_returnEmpty() {
-        Substring substring = new Substring();
         String result = substring.checkEnding(null, null);
+        assertEquals("", result);
+    }
+
+    @Test
+    public void givenEmpty_returnEmpty() {
+        String result = substring.checkEnding("", "");
         assertEquals("", result);
     }
 }
