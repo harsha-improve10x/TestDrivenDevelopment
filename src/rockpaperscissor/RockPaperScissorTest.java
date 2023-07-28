901b0a5f63c3149c795f818873c520b4d59636a7
Harsha <shreeharsha.a@improve10x.com>
2023-07-28 11:30:39 +0530

    Tested for givenBothNull_returnNull

--- src/rockpaperscissor/RockPaperScissorTest.java
+++ src/rockpaperscissor/RockPaperScissorTest.java
@@ -2,8 +2,17 @@ package rockpaperscissor;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class RockPaperScissorTest {
 
     @Test
     public void nothing() {}
+
+    @Test
+    public void givenBothNull_returnNull() {
+        RpsGame rpsGame = new RpsGame();
+        String result = rpsGame.rps(null, null);
+        assertEquals("", result);
+    }
 }
---
+++ src/rockpaperscissor/RpsGame.java
@@ -0,0 +1,7 @@
+package rockpaperscissor;
+
+public class RpsGame {
+    public String rps(String player1, String player2) {
+        return "";
+    }
+}
