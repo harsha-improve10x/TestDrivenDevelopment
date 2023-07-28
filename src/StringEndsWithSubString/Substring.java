720fc2be71897474f3fd129ae4a80e35b3ba7d49
Harsha <shreeharsha.a@improve10x.com>
2023-07-28 15:06:10 +0530

    givenNull_returnEmpty

---
+++ src/StringEndsWithSubString/Substring.java
@@ -0,0 +1,9 @@
+package StringEndsWithSubString;
+
+public class Substring {
+
+
+    public String checkEnding(String s1, String s2) {
+        return "";
+    }
+}
--- src/StringEndsWithSubString/SubstringTest.java
+++ src/StringEndsWithSubString/SubstringTest.java
@@ -2,8 +2,17 @@ package StringEndsWithSubString;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SubstringTest {
 
     @Test
     public void nothing() {}
+
+    @Test
+    public void givenNull_returnEmpty() {
+        Substring substring = new Substring();
+        String result = substring.checkEnding(null, null);
+        assertEquals("", result);
+    }
 }
