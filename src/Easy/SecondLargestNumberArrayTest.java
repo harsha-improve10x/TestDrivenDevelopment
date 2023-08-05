723cc9b6df8388fe4b9fe75af7d590d67202dc32
Harsha <shreeharsha.a@improve10x.com>
2023-08-05 11:45:04 +0530

    Tested for givenEmpty_returnZero

--- src/Easy/SecondLargestNumberArrayTest.java
+++ src/Easy/SecondLargestNumberArrayTest.java
@@ -1,18 +1,30 @@
 package Easy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SecondLargestNumberArrayTest {
+    private SecondLargest secondLargest;
 
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        secondLargest = new SecondLargest();
+    }
+
     @Test
     public void givenNull_returnZero() {
-        SecondLargest secondLargest = new SecondLargest();
         int result = secondLargest.findSecondLargest(null);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void givenEmpty_returnZero() {
+        int result = secondLargest.findSecondLargest(new int[]{});
         assertEquals(0, result);
     }
 
