e71ff94086502135e6a4dcd6fb393ec6a02b66f5
Harsha <shreeharsha.a@improve10x.com>
2023-08-05 11:02:23 +0530

    Tested for givenNull_returnZero

--- src/Easy/FindLargestNumberInArrayTest.java
+++ src/Easy/FindLargestNumberInArrayTest.java
@@ -2,8 +2,17 @@ package Easy;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class FindLargestNumberInArrayTest {
 
     @Test
     public void nothing() {}
+
+    @Test
+    public void givenNull_returnZero() {
+        LargestNumberArray largestNumberArray = new LargestNumberArray();
+        int result = largestNumberArray.findLargest(null);
+        assertEquals(0, result);
+    }
 }
---
+++ src/Easy/LargestNumberArray.java
@@ -0,0 +1,7 @@
+package Easy;
+
+public class LargestNumberArray {
+    public int findLargest(int[] arr) {
+        return 0;
+    }
+}
