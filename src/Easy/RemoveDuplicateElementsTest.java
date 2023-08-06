88451e0ff19aa0a7fc517a2b85b8aec2f7b34286
Harsha <shreeharsha.a@improve10x.com>
2023-08-06 11:36:19 +0530

    Tested for givenZero_returnZero

--- src/Easy/RemoveDuplicateElementsTest.java
+++ src/Easy/RemoveDuplicateElementsTest.java
@@ -1,25 +1,36 @@
 package Easy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 
 public class RemoveDuplicateElementsTest {
+    private RemoveDuplicateElements removeDuplicateElements;
 
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    private void setup() {
+        removeDuplicateElements = new RemoveDuplicateElements();
+    }
+
     @Test
     public void givenNull_returnZero() {
-        RemoveDuplicateElements removeDuplicateElements = new RemoveDuplicateElements();
         int[] result = removeDuplicateElements.remove(null);
         assertArrayEquals(new int[]{0}, result);
     }
 
     @Test
     public void givenEmpty_returnZero() {
-        RemoveDuplicateElements removeDuplicateElements = new RemoveDuplicateElements();
         int[] result = removeDuplicateElements.remove(new int[]{});
+        assertArrayEquals(new int[]{0}, result);
+    }
+
+    @Test
+    public void givenZero_returnZero() {
+        int[] result = removeDuplicateElements.remove(new int[]{0});
         assertArrayEquals(new int[]{0}, result);
     }
 }
