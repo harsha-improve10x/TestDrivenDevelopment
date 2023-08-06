f81e8fe25f0b6bb556ea75f3b795e3874746c080
Harsha <shreeharsha.a@improve10x.com>
2023-08-06 19:36:43 +0530

    Tested for givenEmpty_returnZero

--- src/Easy/LeftRotateArrayTest.java
+++ src/Easy/LeftRotateArrayTest.java
@@ -1,18 +1,30 @@
 package Easy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertArrayEquals;
 
 public class LeftRotateArrayTest {
+    private LeftRotateArray leftRotateArray;
 
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        leftRotateArray = new LeftRotateArray();
+    }
+
     @Test
-    public void givenNull_returnNull() {
-        LeftRotateArray leftRotateArray = new LeftRotateArray();
+    public void givenNull_returnZero() {
         int[] result = leftRotateArray.rotate(null);
+        assertArrayEquals(new int[]{0}, result);
+    }
+
+    @Test
+    public void givenEmpty_returnZero() {
+        int[] result = leftRotateArray.rotate(new int[]{});
         assertArrayEquals(new int[]{0}, result);
     }
 }
