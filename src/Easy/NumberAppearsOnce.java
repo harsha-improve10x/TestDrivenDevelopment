7a63eefc4c1f7a83f773b2e6ca4a655c91c39a4c
Harsha <shreeharsha.a@improve10x.com>
2023-08-07 12:57:48 +0530

    Tested for givenNull_returnZero

---
+++ src/Easy/NumberAppearsOnce.java
@@ -0,0 +1,7 @@
+package Easy;
+
+public class NumberAppearsOnce {
+    public int countAppears(int[] arr) {
+        return 0;
+    }
+}
--- src/Easy/NumberAppearsOnceTest.java
+++ src/Easy/NumberAppearsOnceTest.java
@@ -2,8 +2,17 @@ package Easy;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class NumberAppearsOnceTest {
 
     @Test
     public void nothing() {}
+
+    @Test
+    public void givenNull_returnZero() {
+        NumberAppearsOnce numberAppearsOnce = new NumberAppearsOnce();
+        int result = numberAppearsOnce.countAppears(null);
+        assertEquals(0, result);
+    }
 }
