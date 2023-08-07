b985d6a8c1ed3d908f7f90df0c926e49367f06f6
Harsha <shreeharsha.a@improve10x.com>
2023-08-07 11:56:36 +0530

    Tested for givenNull_returnZero

---
+++ src/Easy/MaximumConsecutiveOne.java
@@ -0,0 +1,7 @@
+package Easy;
+
+public class MaximumConsecutiveOne {
+    public int maximum(int[] arr) {
+        return 0;
+    }
+}
--- src/Easy/MaximumConsecutiveOnesTest.java
+++ src/Easy/MaximumConsecutiveOnesTest.java
@@ -2,9 +2,18 @@ package Easy;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class MaximumConsecutiveOnesTest {
 
     @Test
     public void nothing() {}
+
+    @Test
+    public void givenNull_returnZero() {
+        MaximumConsecutiveOne maximumConsecutiveOne = new MaximumConsecutiveOne();
+        int result = maximumConsecutiveOne.maximum(null);
+        assertEquals(0, result);
+    }
 
 }
