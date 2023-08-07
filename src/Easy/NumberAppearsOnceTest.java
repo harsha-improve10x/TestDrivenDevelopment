5b1bbe6ceccd1b83107987a778a6883c3fd39acd
Harsha <shreeharsha.a@improve10x.com>
2023-08-07 12:59:49 +0530

    Tested for givenEmpty_returnZero

--- src/Easy/NumberAppearsOnceTest.java
+++ src/Easy/NumberAppearsOnceTest.java
@@ -1,18 +1,29 @@
 package Easy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class NumberAppearsOnceTest {
-
+    private NumberAppearsOnce numberAppearsOnce;
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        numberAppearsOnce = new NumberAppearsOnce();
+    }
+
     @Test
     public void givenNull_returnZero() {
-        NumberAppearsOnce numberAppearsOnce = new NumberAppearsOnce();
         int result = numberAppearsOnce.countAppears(null);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void givenEmpty_returnZero() {
+        int result = numberAppearsOnce.countAppears(new int[]{});
         assertEquals(0, result);
     }
 }
