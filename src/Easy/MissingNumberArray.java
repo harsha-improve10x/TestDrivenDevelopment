5294e24aa2cdbd8b272d750076f759ec28c73aba
Harsha <shreeharsha.a@improve10x.com>
2023-08-07 11:13:30 +0530

    Tested for givenOneThree_returnTwo

--- src/Easy/MissingNumberArray.java
+++ src/Easy/MissingNumberArray.java
@@ -7,6 +7,13 @@ public class MissingNumberArray {
             return result;
         } else if (arr.length == 1) {
             result = 1;
+        } else {
+            int missingNumber = n * (n+1)/2;
+            int number = 0;
+            for (int i = 0; i < n-1; i++) {
+                number += arr[i];
+            }
+            result = missingNumber - number;
         }
         return result;
     }
--- src/Easy/MissingNumberTest.java
+++ src/Easy/MissingNumberTest.java
@@ -33,4 +33,10 @@ public class MissingNumberTest {
         int missing = missingNumberArray.findMissingNumber(new int[]{1}, 1);
         assertEquals(1, missing);
     }
+
+    @Test
+    public void givenOneThree_returnTwo() {
+        int missing = missingNumberArray.findMissingNumber(new int[]{1, 3}, 3);
+        assertEquals(2, missing);
+    }
 }
