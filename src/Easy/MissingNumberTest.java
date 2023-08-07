f80bae1de0dfcc2b0f515b0e91a32c6935073a93
Harsha <shreeharsha.a@improve10x.com>
2023-08-07 10:58:30 +0530

    Tested for givenEmpty_returnZero

--- src/Easy/MissingNumberTest.java
+++ src/Easy/MissingNumberTest.java
@@ -1,19 +1,30 @@
 package Easy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class MissingNumberTest {
+    private  MissingNumberArray missingNumberArray;
 
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        missingNumberArray = new MissingNumberArray();
+    }
+
     @Test
     public void givenNull_returnZero() {
-        MissingNumberArray missingNumberArray = new MissingNumberArray();
         int missing = missingNumberArray.findMissingNumber(null, 0);
         assertEquals(0, missing);
     }
 
+    @Test
+    public void givenEmpty_returnZero() {
+        int missing = missingNumberArray.findMissingNumber(new int[]{}, 0);
+        assertEquals(0, missing);
+    }
 }
