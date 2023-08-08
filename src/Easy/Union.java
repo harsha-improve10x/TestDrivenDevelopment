d8a4e8d4076973eca9d844d789c90c44eae9b126
Harsha <shreeharsha.a@improve10x.com>
2023-08-08 15:11:17 +0530

    Tested for givenNullNull_returnEmpty

--- src/Easy/Union.java
+++ src/Easy/Union.java
@@ -1,4 +1,10 @@
 package Easy;
 
+import java.util.ArrayList;
+
 public class Union {
+    public ArrayList<Integer> makeUnion(int[] arr1, int[] arr2) {
+        ArrayList<Integer> unionList = new ArrayList();
+        return unionList;
+    }
 }
--- src/Easy/UnionTest.java
+++ src/Easy/UnionTest.java
@@ -2,9 +2,19 @@ package Easy;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class UnionTest {
 
     @Test
     public void nothing() {}
 
+    @Test
+    public void givenNullNull_returnEmpty() {
+        Union union = new Union();
+        ArrayList<Integer> list = union.makeUnion(null, null);
+        assertEquals(new ArrayList<Integer>(){}, list);
+    }
 }
