edef39c2e1fc7538605dad6c35a329076d8e3048
Harsha <shreeharsha.a@improve10x.com>
2023-08-10 17:06:49 +0530

    Tested for givenEmpty_returnEmpty

--- src/Easy/LeadersInArrayTest.java
+++ src/Easy/LeadersInArrayTest.java
@@ -1,5 +1,6 @@
 package Easy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -7,13 +8,24 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 
 public class LeadersInArrayTest {
+    private  LeadersInArray leadersInArray;
 
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        leadersInArray = new LeadersInArray();
+    }
+
     @Test
     public void givenNull_returnEmpty() {
-        LeadersInArray leadersInArray = new LeadersInArray();
+        ArrayList<Integer> arrayList = leadersInArray.leaders(null);
+        assertEquals(new ArrayList<Integer>(), arrayList);
+    }
+
+    @Test
+    public void givenEmpty_returnEmpty() {
         ArrayList<Integer> arrayList = leadersInArray.leaders(null);
         assertEquals(new ArrayList<Integer>(), arrayList);
     }
