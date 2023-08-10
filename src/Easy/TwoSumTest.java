024060d861e487ad266fa8321d9a5290f9c0cf36
Harsha <shreeharsha.a@improve10x.com>
2023-08-10 10:48:03 +0530

    Tested for givenEmpty_targetZero_return

--- src/Easy/TwoSumTest.java
+++ src/Easy/TwoSumTest.java
@@ -1,18 +1,30 @@
 package Easy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class TwoSumTest {
+    private TwoSum twoSum;
 
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        twoSum = new TwoSum();
+    }
+
     @Test
     public void givenNull_targetZero_return() {
-        TwoSum twoSum = new TwoSum();
         String result = twoSum.findSum(null, 0);
+        assertEquals("No", result);
+    }
+
+    @Test
+    public void givenEmpty_targetZero_return() {
+        String result = twoSum.findSum(new int[]{}, 0);
         assertEquals("No", result);
     }
 }
