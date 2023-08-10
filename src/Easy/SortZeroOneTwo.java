b1f6c5ad03c7dc1c54b980adfbb96372c2040046
Harsha <shreeharsha.a@improve10x.com>
2023-08-10 12:11:51 +0530

    Tested for givenNull_returnEmpty

---
+++ src/Easy/SortZeroOneTwo.java
@@ -0,0 +1,8 @@
+package Easy;
+
+public class SortZeroOneTwo {
+    public int[] sortArray(int[] arr) {
+        int[] result = {};
+        return result;
+    }
+}
--- src/Easy/SortZeroOneTwoTest.java
+++ src/Easy/SortZeroOneTwoTest.java
@@ -2,8 +2,17 @@ package Easy;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+
 public class SortZeroOneTwoTest {
 
     @Test
     public void nothing() {}
+
+    @Test
+    public void givenNull_returnEmpty() {
+        SortZeroOneTwo sortZeroOneTwo = new SortZeroOneTwo();
+        int[] result = sortZeroOneTwo.sortArray(null);
+        assertArrayEquals(new int[]{}, result);
+    }
 }
