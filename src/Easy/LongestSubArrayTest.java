69c777d75cf973a1008e9868cd133422b9f163ce
Harsha <shreeharsha.a@improve10x.com>
2023-08-11 11:42:35 +0530

    Tested for givenEmpty_returnZero

--- src/Easy/LongestSubArrayTest.java
+++ src/Easy/LongestSubArrayTest.java
@@ -1,18 +1,30 @@
 package Easy;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class LongestSubArrayTest {
+    private LongestSubArray longestSubArray;
 
     @Test
     public void nothing() {}
 
+    @BeforeEach
+    public void setup() {
+        longestSubArray = new LongestSubArray();
+    }
+
     @Test
     public void givenNull_returnZero() {
-        LongestSubArray longestSubArray = new LongestSubArray();
         int result = longestSubArray.findLongest(null, 0);
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void givenEmpty_returnZero() {
+        int result = longestSubArray.findLongest(new int[]{}, 0);
         assertEquals(0, result);
     }
 }
